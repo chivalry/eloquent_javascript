216a3d031db2b44ae74b77d1984fcb91309617f8
Charles Ross <chivalry@mac.com>
2020-04-23 12:27:49 -0700

    Acc is_even.js

---
+++ is_even.js
@@ -0,0 +1,16 @@
+const isEven = function(num) {
+    if (num < 0) {
+        return isEven(num * -1);
+    }
+    if (num == 0) {
+        return true;
+    }
+    if (num == 1) {
+        return false;
+    }
+    return isEven(num - 2);
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
